c9765196ce69fe6fc8e7456b90ac3bb8e1d5bd53
ronalfonso <raalfonsoparra@gmail.com>
2023-05-11 11:25:27 -0400

    correccion en ocultar register

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
     // BASE_URL: 'http://localhost:3000',
-    BASE_URL: 'https://3449-38-166-57-193.ngrok-free.app',
+    BASE_URL: 'https://7103-2803-d700-daf8-e1e0-883d-dd4f-39ba-54e1.ngrok-free.app',
 }
